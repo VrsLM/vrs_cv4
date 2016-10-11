0f43dafea02be56db375fa613cd4263d63654c3a
VrsLM <lukasmartis320@gmail.com>
2016-10-11 17:35:04 +0200

    Vzorový kód pre nastavenie ADC

--- src/main.c
+++ src/main.c
@@ -1,31 +1,75 @@
-
 /* Includes */
 #include <stddef.h>
 #include "stm32l1xx.h"
 
+void led_init(void) {
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+	GPIO_InitTypeDef GPIO_InitStruct;
 
-/* Private typedef */
-/* Private define  */
-/* Private macro */
-/* Private variables */
-/* Private function prototypes */
-/* Private functions */
-
-
-int main(void)
-{
-  int i = 0;
-
-
-  /* TODO - Add your application code here */
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_40MHz;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_5;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
 
 
-  /* Infinite loop */
-  while (1)
-  {
-	i++;
-  }
-  return 0;
+void adc_init(void) {
+	GPIO_InitTypeDef GPIO_InitStructure;
+	ADC_InitTypeDef ADC_InitStructure;
+	/* Enable GPIO clock */
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE); //Opraviù a upraviù
+	/* Configure ADCx Channel 2 as analog input */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	/* Enable the HSI oscillator */
+	RCC_HSICmd(ENABLE);
+	/* Check that HSI oscillator is ready */
+	while (RCC_GetFlagStatus(RCC_FLAG_HSIRDY) == RESET)
+		;
+	/* Enable ADC clock */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+	/* Initialize ADC structure */
+	ADC_StructInit(&ADC_InitStructure);
+	/* ADC1 configuration */
+	ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
+	ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
+	ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+	ADC_InitStructure.ADC_NbrOfConversion = 1;
+	ADC_Init(ADC1, &ADC_InitStructure);
+	/* ADCx regular channel8 configuration */
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_2, 1, ADC_SampleTime_16Cycles);
+	/* Enable the ADC */
+	ADC_Cmd(ADC1, ENABLE);
+	/* Wait until the ADC1 is ready */
+	while (ADC_GetFlagStatus(ADC1, ADC_FLAG_ADONS) == RESET) {
+	}
+	/* Start ADC Software Conversion */
+	ADC_SoftwareStartConv(ADC1);
+}
+
+uint16_t  AD_value;
+
+int main(void) {
+	adc_init();
+	led_init();
+
+	GPIO_WriteBit(GPIOA, GPIO_Pin_5, Bit_SET);
+	while (1) {
+
+		/* Start ADC Software Conversion */
+		ADC_SoftwareStartConv(ADC1);
+		while (!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC)) {
+		}
+		AD_value = ADC_GetConversionValue(ADC1);
+		printf("ad_value = %d", AD_value);
+
+	}
+	return 0;
 }
 
 #ifdef  USE_FULL_ASSERT
